b965acb6333ce1959a01b1736a69f8ace2772231
xRetart <richard.network96@gmail.com>
2023-10-06 20:28:58 +0200

    implement horizontal srolling for query prompt

--- src/interface/ui/query.rs
+++ src/interface/ui/query.rs
@@ -15,11 +15,21 @@ impl Query {
     }
     pub fn render<B: Backend>(&self, frame: &mut Frame<B>, destination: Rect) {
         let block = Block::new().borders(Borders::ALL).border_type(BorderType::Thick);
-        let widget = Paragraph::new(self.string.clone()).block(block);
+        let scroll = self.scroll(destination.width);
+        let widget = Paragraph::new(self.string.clone()).block(block).scroll((0, scroll));
         frame.render_widget(widget, destination);
 
-        let x = destination.x + u16::try_from(self.string.len()).unwrap() + 1;
+        let x = destination.x + self.cursor(destination.width);
         let y = destination.y + 1;
         frame.set_cursor(x, y);
+    }
+    fn cursor(&self, width: u16) -> u16 {
+        let length = u16::try_from(self.string.len()).unwrap();
+        (length + 1).min(width - 3)
+    }
+    fn scroll(&self, width: u16) -> u16 {
+        let length = i64::try_from(self.string.len()).unwrap();
+        let scroll = (length - i64::from(width) + 4).max(0);
+        u16::try_from(scroll).unwrap()
     }
 }
